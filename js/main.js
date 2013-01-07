4d0a7008262fd2d3d884814e7a0609777ca01aa5
xavier Bekaert <xavier24@hotmail.com>
2013-01-07 15:39:35 +0100

    mise en place JS header+nav

--- js/main.js
+++ js/main.js
@@ -6,13 +6,32 @@
 	"use strict";
 
 	// -- globals
-		
+		var $onget_recherche,
+			$barre_recherche,
+			$resultats,
+			$listElement;
 
 	// -- methods
+		var showSearch = function(){
+			if($barre_recherche.is(':visible')){
+				$barre_recherche.css('display','none');
+			}
+			else{
+				$barre_recherche.css('display','block');
+			}
+		}
 		
 	$( function () {
 
 		// -- onload routines
+		$onget_recherche = $('nav .search');
+		$barre_recherche = $('#search');
+		$resultats = $('#resultats');
+		$listElement = $resultats.find('.resultat').first().remove();
+		
+		$barre_recherche.css('display','none');
+		
+		$onget_recherche.on('click',showSearch);
 		
 	} );
 
