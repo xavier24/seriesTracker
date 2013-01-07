1f8addb4587efdc7caf8d2e1718663628418174e
xavier Bekaert <xavier24@hotmail.com>
2013-01-07 20:19:15 +0100

    serie populaire accueil

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php include('header.php') ?>
+<div id="populaires">
+    <div class="populaire">
+        <img class="banner" width="100%" />
+        <p class="titre"></p>
+        <p class="saison"></p>
+        <p class="suivi"></p>
+        <p class="note"></p>
+    </div>
+</div>
 <div id="resultats">
-	<div class="resultat">
-		<p class="titre"></p>
-	</div>
+    <div class="resultat">
+            <p class="titre"></p>
+    </div>
 </div>
 <?php include('footer.php') ?>
--- js/main.js
+++ js/main.js
@@ -6,44 +6,68 @@
 	"use strict";
 
 	// -- globals
-		var $onget_recherche,
-			$barre_recherche,
-			$resultats,
-			$listElement;
+        var $onget_recherche,
+            $barre_recherche,
+            $populaires,
+            $listPopular,
+            $resultats,
+            $listElement;
 
 	// -- methods
-		var showSearch = function(){
-			if($barre_recherche.is(':visible')){
-				$barre_recherche.css('display','none');
-			}
-			else{
-				$barre_recherche.css('display','block');
-			}
-		}
-		var lister = function(){
-			$.ajax({
-				url: "http://api.betaseries.com/shows/display/all.json?key=af181b000037",
-				dataType: "jsonp",
-				success: function(data){
-				console.log(data.root.shows.length);
-					for( var i=0; i<10; i++){
-						var $currentElement = $listElement.clone(true);
-						$currentElement.attr('id',data.root.shows[i].url);
-						$currentElement.find('p').html(data.root.shows[i].title);
-						
-						$currentElement.appendTo($resultats);
-					//$resultats.child[i].find('.titre').html(data.root.shows[i].title);
-					
-					}
-				}
-			})
-		}//lister
+                var showSearch = function(){
+                    if($barre_recherche.is(':visible')){
+                        $barre_recherche.css('display','none');
+                    }
+                    else{
+                        $barre_recherche.css('display','block');
+                    }
+                }
+                var index =function(){
+                    $.ajax({
+                        url: "http://127.0.0.1/seriesTracker/json/populaire.json",
+                        dataType: "json",
+                        success: function(data){
+                        console.log(data.root.shows[1].url);
+                        console.log('dd');
+                            for(var i=0; i<4; i++){
+                                var $currentElement = $listPopular.clone(true);
+                                $currentElement.attr('id',data.root.shows[i].url);
+                                $currentElement.find('img').attr('src',data.root.shows[i].banner).attr('alt','photo de la serie '+data.root.shows[i].title).attr('title','photo de la serie '+data.root.shows[i].title);
+                                $currentElement.find('.titre').html(data.root.shows[i].title);
+                                $currentElement.find('.saison').html(data.root.shows[i].season+' saisons - '+data.root.shows[i].episodes+' épisodes');
+                                $currentElement.find('.suivi').html("suivie par "+data.root.shows[i].suivie+" membres");
+                                $currentElement.find('.note').html(data.root.shows[i].note.mean+"/5");
+                                $currentElement.appendTo($populaires);
+                            }
+                        }
+                    })                    
+                }
+                var lister = function(){
+                    $.ajax({
+                        url: "http://api.betaseries.com/shows/display/all.json?key=af181b000037",
+                        dataType: "jsonp",
+                        success: function(data){
+                        console.log(data.root.shows.length);
+                            for( var i=0; i<10; i++){
+                                var $currentElement = $listElement.clone(true);
+                                $currentElement.attr('id',data.root.shows[i].url);
+                                $currentElement.find('p').html(data.root.shows[i].title);
+
+                                $currentElement.appendTo($resultats);
+                            //$resultats.child[i].find('.titre').html(data.root.shows[i].title);
+
+                            }
+                        }
+                    })
+                }//lister
 		
 	$( function () {
 
 		// -- onload routines
 		$onget_recherche = $('nav .search');
 		$barre_recherche = $('#search');
+                $populaires = $('#populaires');
+		$listPopular = $populaires.find('.populaire').first().remove();
 		$resultats = $('#resultats');
 		$listElement = $resultats.find('.resultat').first().remove();
 		
@@ -51,7 +75,7 @@
 		
 		$onget_recherche.on('click',showSearch);
 		
-		lister();
+		index();
 	} );
 
 }( jQuery ) );
