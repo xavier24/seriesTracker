1ca744afc214ce70b74bc70dc3637a3ca9c5ce00
xavier Bekaert <xavier24@hotmail.com>
2013-01-07 15:30:05 +0100

    mise en place de base

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*jslint regexp: true, vars: true, white: true, browser: true */
+/*jshint nonstandard: true, browser: true, boss: true */
+/*global jQuery */
+
+( function ( $ ) {
+	"use strict";
+
+	// -- globals
+		
+
+	// -- methods
+		
+	$( function () {
+
+		// -- onload routines
+		
+	} );
+
+}( jQuery ) );
+
